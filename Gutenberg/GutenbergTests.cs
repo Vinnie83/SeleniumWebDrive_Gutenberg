a70db5c207ce2e02a334b52d5a03c06abcd4bed2
Vinnie83 <122921599+Vinnie83@users.noreply.github.com>
2023-11-18 16:19:24 +0200

    Add project files.

---
+++ Gutenberg/GutenbergTests.cs
@@ -0,0 +1,49 @@
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+
+namespace Gutenberg
+{
+    public class GutenbergTests
+    {
+        private WebDriver driver;
+
+
+
+        [SetUp]
+        public void Setup()
+        {
+
+            this.driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+            
+
+        }
+
+        [TearDown]
+
+        public void TearDown()
+        {
+            driver.Quit();  
+        }
+
+        [Test]
+        public void Test_SearchBook()
+        {
+            driver.Url = "https://www.gutenberg.org/";
+
+            var searchButton = driver.FindElement(By.Id("menu-book-search"));
+            searchButton.Clear();
+            searchButton.SendKeys("Tom Sawyer");
+
+            var goButton = driver.FindElement(By.Name("submit_search"));
+            goButton.Click();
+
+            Thread.Sleep(3000);
+
+            var firstResult = driver.FindElement(By.XPath("//*[@id=\"content\"]/div[2]/div/ul/li[5]/a"));
+            
+            Assert.That(firstResult.Text, Is.EqualTo("The Adventures of Tom Sawyer, Complete\r\nMark Twain\r\n9989 downloads"));
+            Assert.That(firstResult.Text.Contains("Tom Sawyer"));
+        }
+    }
+}
