195c6f75e53b43910d88f3f34926e5168a0c262b
Vinnie83 <122921599+Vinnie83@users.noreply.github.com>
2023-11-19 20:08:44 +0200

    Added test

--- Gutenberg/GutenbergTests.cs
+++ Gutenberg/GutenbergTests.cs
@@ -45,5 +45,25 @@ namespace Gutenberg
             Assert.That(firstResult.Text, Is.EqualTo("The Adventures of Tom Sawyer, Complete\r\nMark Twain\r\n9989 downloads"));
             Assert.That(firstResult.Text.Contains("Tom Sawyer"));
         }
+
+        [Test]
+        public void Test_SearchNonExistingBook()
+        {
+            driver.Url = "https://www.gutenberg.org/";
+
+            var searchButton = driver.FindElement(By.Id("menu-book-search"));
+            searchButton.Clear();
+            searchButton.SendKeys("bebebebebe");
+
+            var goButton = driver.FindElement(By.Name("submit_search"));
+            goButton.Click();
+
+            Thread.Sleep(3000);
+
+            var noRecord = driver.FindElement(By.ClassName("title"));
+
+            Assert.That(noRecord.Text, Is.EqualTo("No records found."));
+        }
+
     }
 }
